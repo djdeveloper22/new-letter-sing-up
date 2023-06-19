4e838d01693280cb74dff52c5a1b044c74e35ae4
djdeveloper22 <115105022+djdeveloper22@users.noreply.github.com>
2023-06-19 12:07:44 -0500

    version 1 newsletter sign-up

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, '/src/index.js')
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'mi site',
+      template: './src/index.html'
+    }),
+  ],
+};
